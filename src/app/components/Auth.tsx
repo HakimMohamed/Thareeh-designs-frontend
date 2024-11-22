b8201b5fd690e42d485c675df5a586dc276d4071
HakimMohamed <kemstroarab@gmail.com>
2024-11-22 14:36:22 +0200

    fix auth modal not closing when clicking on x icon

--- src/app/components/Auth.tsx
+++ src/app/components/Auth.tsx
@@ -6,7 +6,6 @@ import {
   ModalBody,
   ModalFooter,
   Button,
-  useDisclosure,
   Checkbox,
   Input,
   Link,
@@ -14,72 +13,80 @@ import {
 import { MailIcon, LockIcon } from "./icons/Icons";
 import { useIsAuthenticated } from "../stores/auth-model";
 
-export default function Auth() {
-  const { onOpenChange } = useDisclosure();
-  const { isOpen, setIsOpen } = useIsAuthenticated();
+export default function App() {
+  const { setIsOpen, isOpen } = useIsAuthenticated();
 
   return (
-    <>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
-              <ModalBody>
-                <Input
-                  autoFocus
-                  endContent={
-                    <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                  }
-                  label="Email"
-                  placeholder="Enter your email"
-                  variant="bordered"
-                />
-                <Input
-                  endContent={
-                    <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
-                  }
-                  label="Password"
-                  placeholder="Enter your password"
-                  type="password"
-                  variant="bordered"
-                />
-                <div className="flex py-2 px-1 justify-between">
-                  <Checkbox
-                    classNames={{
-                      label: "text-small",
-                    }}
-                  >
-                    Remember me
-                  </Checkbox>
-                  <Link color="primary" href="#" size="sm">
-                    Forgot password?
-                  </Link>
-                </div>
-              </ModalBody>
-              <ModalFooter>
-                <Button
-                  color="danger"
-                  variant="flat"
-                  onPress={() => setIsOpen(false)}
-                >
-                  Close
-                </Button>
-                <Button
-                  color="primary"
-                  onPress={() => {
-                    // Perform authentication logic here, e.g., set auth token in local storage
-                    localStorage.setItem("token", "your-auth-token");
-                    onClose(); // Close the modal after signing in
+    <Modal
+      isOpen={isOpen}
+      onOpenChange={(open) => {
+        if (!open) {
+          setIsOpen(false);
+        }
+      }}
+      placement="top-center"
+    >
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+            <ModalBody>
+              <Input
+                autoFocus
+                endContent={
+                  <MailIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                }
+                label="Email"
+                placeholder="Enter your email"
+                variant="bordered"
+              />
+              <Input
+                endContent={
+                  <LockIcon className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                }
+                label="Password"
+                placeholder="Enter your password"
+                type="password"
+                variant="bordered"
+              />
+              <div className="flex py-2 px-1 justify-between">
+                <Checkbox
+                  classNames={{
+                    label: "text-small",
                   }}
                 >
-                  Sign in
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
-    </>
+                  Remember me
+                </Checkbox>
+                <Link color="primary" href="#" size="sm">
+                  Forgot password?
+                </Link>
+              </div>
+            </ModalBody>
+            <ModalFooter>
+              <Button
+                color="danger"
+                variant="flat"
+                onPress={() => {
+                  setIsOpen(false);
+                  onClose();
+                }}
+              >
+                Close
+              </Button>
+              <Button
+                color="primary"
+                onPress={() => {
+                  setIsOpen(false);
+                  onClose();
+                }}
+              >
+                Sign in
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
   );
 }
+// const { isOpen, setIsOpen } = useIsAuthenticated();
