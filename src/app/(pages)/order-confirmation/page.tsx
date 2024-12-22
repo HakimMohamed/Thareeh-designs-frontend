b28183c4b5424e06889a643c7745ba2e0104ca1b
HakimMohamed <iabdelhakimmohamed@gmail.com>
2024-12-23 01:11:01 +0200

    fix thank you page build

--- src/app/(pages)/order-confirmation/page.tsx
+++ src/app/(pages)/order-confirmation/page.tsx
@@ -1,14 +1,11 @@
 import ThankYouCard from "@/components/ThankYouCard";
 import { OrdersService } from "@/services/order";
-import { Params } from "next/dist/server/request/params";
 import { SearchParams } from "next/dist/server/request/search-params";
 
 export default async function ProductPage(props: {
-  params: Params;
-  searchParams: SearchParams;
+  searchParams: Promise<SearchParams>;
 }) {
   const searchParams = await props.searchParams;
-  console.log(searchParams);
   const order = await OrdersService.getOrderById(searchParams.id as string);
 
   return <ThankYouCard orderId={order._id} />;
